ea9b65e53f291eac3093ab9900b3bb5b756a92a5
aryan2181111 <aryanthombare18@gmail.com>
2026-08-24 22:52:29 +0530

    Add Tram [A] from Codeforces

---
+++ 116/A - Tram/solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+ 
+        int current = 0;
+        int max = 0;
+ 
+        for (int i = 0; i < n; i++) {
+ 
+            int a = sc.nextInt(); // people leaving
+            int b = sc.nextInt(); // people entering
+ 
+            current = current - a + b;
+ 
+            max = Math.max(max, current);
+        }
+ 
+        System.out.println(max);
+ 
+        sc.close();
+    }
+}
