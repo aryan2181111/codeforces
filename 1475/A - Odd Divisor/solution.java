c9259889d150dc4e0d40389c7dae1a1031078f47
aryan2181111 <aryanthombare18@gmail.com>
2026-08-26 23:09:56 +0530

    Add Odd Divisor [A] from Codeforces

---
+++ 1475/A - Odd Divisor/solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+ 
+            long n = sc.nextLong();
+ 
+            while (n % 2 == 0) {
+                n /= 2;
+            }
+ 
+            if (n == 1) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+        }
+ 
+        sc.close();
+    }
+}
