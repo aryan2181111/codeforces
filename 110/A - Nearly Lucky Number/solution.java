163600c1534d6c6f87f49195c785f5eae3e957b8
aryan2181111 <aryanthombare18@gmail.com>
2026-08-26 23:06:55 +0530

    Add Nearly Lucky Number [A] from Codeforces

---
+++ 110/A - Nearly Lucky Number/solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        String n = sc.next();
+ 
+        int luckyCount = 0;
+ 
+        for (int i = 0; i < n.length(); i++) {
+ 
+            char ch = n.charAt(i);
+ 
+            if (ch == '4' || ch == '7') {
+                luckyCount++;
+            }
+        }
+ 
+        if (luckyCount == 4 || luckyCount == 7) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+ 
+        sc.close();
+    }
+}
